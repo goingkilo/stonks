76438b89f3fda306a6eb0db75073721bd00e9e25
karthik raghunathan <kraghunathan@nile-global.com>
2020-05-19 10:20:44 +0530

    first cut

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import csv
+from datetime import datetime
+import datetime
 
 cols = 'Date,Open,High,Low,Close,Adj Close,Volume'.split(',')
 
@@ -8,12 +10,21 @@ def load(filename):
 		f1 = csv.reader( f, delimiter=',')
 		for i in f1:
 			data.append(i)
+
 	return data
 
 
+def to_map(d):
+	r = {}
+	for i in d:
+		key = i[0]
+		value = i
+		r[key] = i
+	return r
+		
 
-def get(key,data_row):
-	return data_row[ cols.index( key))
+def  get(key,data_row):
+	return data_row[ cols.index( key)]
 
 
 g_date = lambda  r:get( 'Date', r)
@@ -23,7 +34,28 @@ g_low = lambda  r:get( 'Low', r)
 g_close = lambda  r:get( 'Close', r)
 g_vol = lambda  r:get( 'Volume', r)
 	
+def transaction( historical_prices, buy_date, sell_date, qty):
+	buy_price = historical_prices.get[buy_date]  
+	sell_price = historical_prices.get[sell_date]  
+	return qty * (sell_price - buy_price)
+
+
+def to_date(d):
+	x = datetime.strptime( d, '%Y-%m-%d')
+	return x
+
 
 
 a = load( 'RELIANCE.NS.all.csv')
 print( 'loaded ' , len(a), ' rows')
+
+
+b = to_map(a)
+
+dates = [x[0] for x in a]
+print( dates[0])
+print( dates[1])
+print( dates[-1])
+
+
+
